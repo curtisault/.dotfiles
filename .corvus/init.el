9d45b5f46f51376e40445b0e277ce96a990a9cd4
Curtis Ault <curtisault@gmail.com>
2021-03-16 22:02:26 -0500

    Create init.el

---
+++ .corvus/init.el
@@ -0,0 +1,13 @@
+;; Minimal UI
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode  -1)
+(tool-bar-mode    -1)
+(tooltip-mode     -1)
+(global-linum-mode 1)
+
+;; Font
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-14"))
+(add-to-list 'default-frame-alist '(line-spacing . 0.2))
+
+(setq visual-bell t)
