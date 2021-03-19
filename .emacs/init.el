4866c50e73a21487a89b87e15a9c778d5edeb26f
Curtis Ault <curtisault@gmail.com>
2021-03-19 01:06:02 -0500

    Update init.el

--- .emacs/init.el
+++ .emacs/init.el
@@ -1,181 +1,136 @@
-
-;; Package configs
-(require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
-			 ("gnu"   . "http://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
-
-
-;; Bootstrap `use-package`
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-
-
 ;; Minimal UI
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message ";; Happy Hacking")
+
+(menu-bar-mode    -1)
 (scroll-bar-mode  -1)
 (tool-bar-mode    -1)
 (tooltip-mode     -1)
 (global-linum-mode 1)
-(add-to-list 'default-frame-alist '(font . "Hack"))
+(column-number-mode )
 
+;; Fancy titlebar for MacOS
 
-;; Vim mode
+;; Font
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-12"))
+(add-to-list 'default-frame-alist '(line-spacing . 0.2))
+
+(setq visual-bell t)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+  ;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 (use-package evil
-  :ensure t
-  :config
+  :config 
   (evil-mode 1))
 
+(use-package org-evil)
 
-;; Theme
 (use-package doom-themes
-  :ensure t
+  :init (load-theme 'doom-vibrant t))
+
+
+(use-package counsel
+  :after ivy
+  :config (counsel-mode))
+
+(use-package ivy
+  :defer 0.1
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :custom
+  (ivy-count-format "(%d/%d) ")
+  (ivy-use-virtual-buffers t)
+  :config (ivy-mode))
+
+(use-package ivy-rich
+  :after ivy
+  :custom
+  (ivy-virtual-abbreviate 'full
+                          ivy-rich-switch-buffer-align-virtual-buffer t
+                          ivy-rich-path-style 'abbrev)
   :config
-  (load-theme 'doom-vibrant t))
+  (ivy-set-display-transformer 'ivy-switch-buffer
+                               'ivy-rich-switch-buffer-transformer))
 
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
 
-;; Helm
-(use-package helm
-  :ensure t
-  :init
-  (setq helm-split-window-inside-p t
-	helm-move-to-line-cycle-in-source t
-	helm-echo-input-in-header-line t
-	helm-autoresize-max-height 0
-	helm-autoresize-min-height 20)
-  :config
-(helm-mode 1))
-
-
-;; Projectile
-(use-package projectile
-  :ensure t
-  :init
-  (setq projectile-require-project-root nil)
-  :config
-  (projectile-mode 1))
-
-
-;; Helm Projectile
-(use-package helm-projectile
-  :ensure t
-  :init
-  (setq helm-projectile-fuzzy-match t)
-  (setq projectile-enable-caching t)
-  :config
-(helm-projectile-on))
-
-
-;; Which Key
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+ 
 (use-package which-key
-  :ensure t
   :init
   (setq which-key-separator " ")
   (setq which-key-prefix-prefix "+")
+  :diminish which-key-mode
   :config
-  (which-key-mode 1))
-
-;; Splash screen
-(setq inhibit-startup-screen t)
-(setq initial-scratch-message ";; Happy Hacking")
+  (which-key-mode)
+  (setq which-key-idle-delay 0.1))
 
 
 ;; Custom keybinding
 (use-package general
-  :ensure t
   :config (general-define-key
   :states '(normal visual insert emacs)
   :prefix "SPC"
   :non-normal-prefix "M-SPC"
-  "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
-  "SPC" '(helm-M-x :which-key "M-x")
+  ":"   '(counsel-M-x :which-key "M-x")
+  "/"   '(counsel-rg :which-key "ripgrep") ; You'll need counsel package for this
+  "["   '(switch-to-prev-buffer :which-key "previous buffer")
+  "]"   '(switch-to-next-buffer :which-key "next buffer")
+  ;; "pf"  '(helm-find-file :which-key "find files")
 
-  ;; Files
-  "ff"  '(helm-find-files :which-key "find files")
-  "fs"  '(save-buffer :which-key "save file")
-
-  ;; Projects
-  "pf"  '(projectile-find-file :which-key "project find file")  
-  "pp"  '(helm-projectile-switch-project :which-key "switch project")
-  "pb"  '(helm-projectile-switch-to-buffer :which-key "switch buffer")
-  "pr"  '(helm-show-kill-ring :which-key "show kill ring")
+  ;; eval
+  "eb"  '(eval-buffer :which-key "eval buffer")
 
   ;; Buffers
-  "bb"  '(helm-mini :which-key "buffers list")
-  "bd"  '(kill-this-buffer :which-key "kill buffer")
+  ;; "bb"  '(list-buffers :which-key "buffers list") TODO: Make this better
+  "fs"  '(save-buffer :which-key "save buffer")
 
   ;; Window
   "wl"  '(windmove-right :which-key "move right")
   "wh"  '(windmove-left :which-key "move left")
   "wk"  '(windmove-up :which-key "move up")
   "wj"  '(windmove-down :which-key "move bottom")
-  "w/"  '(split-window-right :which-key "split right")
-  "w."  '(split-window-below :which-key "split bottom")
+  "wL"  '(split-window-right :which-key "split right")
+  "wJ"  '(split-window-below :which-key "split bottom")
   "wd"  '(delete-window :which-key "delete window")
 
-  ;; UI
-  "ti"  '(text-scale-increase :which-key "text scale increase")
-  "td"  '(text-scale-decrease :which-key "text scale decrease")
-
-  ;; Lisp
-  "le"  '(eval-last-sexp :which-key "emacs-lisp eval line")
-
-  ;; Global
+  ;; Others
   "at"  '(ansi-term :which-key "open terminal")
   "qq"  '(kill-emacs :which-key "quit")
 ))
-
-
-;; Language Deps and Niceties
-(use-package alchemist :ensure t)
-(use-package company :ensure t)
-(use-package elixir-mode :ensure t)
-(use-package flycheck :ensure t)
-(use-package flycheck-mix :ensure t)
-(use-package flycheck-credo :ensure t)
-(use-package ggtags :ensure t)
-(use-package ob-elixir :ensure t)
-(use-package smartparens :ensure t)
-
-
-;; Popup Windows to Mini-buffers
-(use-package popwin :ensure t)
-
-
-;; Powerline
-(use-package spaceline
-  :ensure t
-  :init
-  (setq powerline-default-separator 'slant)
-  :config
-  (spaceline-emacs-theme)
-  (spaceline-toggle-minor-modes-off)
-  (spaceline-toggle-buffer-size-off)
-  (spaceline-toggle-evil-state-on))
-
-
-;; Fancy titlebar for MacOS
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
-(setq ns-use-proxy-icon  nil)
-(setq frame-title-format nil)
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (markdown-mode magit alchemist ag helm-ag which-key use-package helm evil-escape doom-themes))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
