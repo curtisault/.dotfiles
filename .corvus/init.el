81383578ed4a677d6c665f88f2a6824e987dd7a9
Curtis Ault <curtisault@gmail.com>
2021-03-18 21:24:05 -0500

    Update init.el

--- .corvus/init.el
+++ .corvus/init.el
@@ -1,13 +1,65 @@
 ;; Minimal UI
+(setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode  -1)
 (tool-bar-mode    -1)
 (tooltip-mode     -1)
 (global-linum-mode 1)
+(column-number-mode)
 
 ;; Font
-(add-to-list 'default-frame-alist '(font . "JetBrains Mono-14"))
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-12"))
 (add-to-list 'default-frame-alist '(line-spacing . 0.2))
 
 (setq visual-bell t)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+  ;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package doom-themes
+  :init (load-theme 'doom-gruvbox t))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 10)))
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
