fcb84e80f785bdb8b28f093483a17b0f67f93721
Curtis Ault <curtisault@gmail.com>
2021-04-10 01:36:02 -0500

    Update xmobar-single.hs

--- linux/.xmonad/xmobar-single.hs
+++ linux/.xmonad/xmobar-single.hs
@@ -1,4 +1,4 @@
-Config { font = "xft:JetBrainsMono:pixelsize=16:antialias=true:hinting=true"
+Config { font = "xft:FontAwesome:pixelsize=16:antialias=true:hinting=true,JetBrainsMono:pixelsize=16:antialias=true:hinting=true"
        , additionalFonts = []
        , borderColor = "black"
        , border = TopB
@@ -22,26 +22,26 @@ Config { font = "xft:JetBrainsMono:pixelsize=16:antialias=true:hinting=true"
                                        , "--low","darkseagreen4"] 
                                        36000
                     -- , Run Network "lo" ["-L","0","-H","32", "--normal","green","--high","red"] 10
-                    , Run Network "eno2" ["-t", "Net: ↓ <rx>KB | ↑ <tx>KB"
-                                        , "-L", "0", "-H", "32", "--normal", "green", "--high", "green"] 10
-                    , Run Cpu ["-t", "CPU: <vbar><total>%", "-L","3","-H","50", "--normal","green","--high","red"] 10
+                    , Run Network "eno2" ["-t", "   <tx>KB |  <rx>KB" , "-L", "0", "-H", "32", "--normal", "green", "--high", "green"] 10
+                    , Run Cpu ["-t", " <vbar><total>%", "-L","3","-H","50", "--normal","green","--high","red"] 10
                     --, Run MultiCpu ["-t","Cpu: <total0>%<total1>%<total2>%<total3>%<total4>%<total5>%<total6>%<total7>%", "-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10
-                    , Run MultiCoreTemp ["-t", "TEMP: <avgvbar><avg>°C"
+                    , Run MultiCoreTemp ["-t", " <avgvbar><avg>°C"
                                        , "-L", "60", "-H", "80"
                                        , "-l", "green"
                                        , "-n", "yellow"
                                        , "-h", "red",
                        "--", "--mintemp", "20", "--maxtemp", "100"] 50
-                    , Run DiskU [("/", "Nvme: <usedvbar><usedp>%"), ("sda1", "Steam: <usedvbar><usedp>%")]
+                    , Run DiskU [("/", ":  <usedp>%"), ("/sda1", ":  <usedp>%")]
                                 ["-L", "20", "-H", "50", "-m", "1", "-p", "3"]
                                 20
-                    , Run Memory ["-t","RAM: <usedratio>%"] 10
+                    , Run Memory ["-t","  <usedratio>%"] 10
                     , Run Swap [] 10
-                    , Run Com "uname" ["-s","-r"] "" 36000
-                    , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
+                    , Run Com "uname" ["-s", "-r"] "" 36000
+                    , Run Date "%H:%M:%S | %Y-%m-%d %p" "date" 10
                     , Run StdinReader
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ <fc=#ee9a00>%date%</fc> / %cpu% / %multicoretemp% / %memory% / %eno2% / %disku% / %KSAT% / %uname%"
+       , template = "%StdinReader% } <fc=#ee9a00>%date%</fc> \
+       \{  %cpu%  /  %multicoretemp%  /  %memory%  /  %eno2%  /  %disku%  /  %KSAT%  /   %uname%"
        }
