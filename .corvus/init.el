e71fd1f8576d329b5eb8a6b695d51324bc306545
Curtis Ault <curtisault@gmail.com>
2021-06-17 08:19:46 -0500

    Update init.el

--- .corvus/init.el
+++ .corvus/init.el
@@ -146,3 +146,18 @@
   "at"  '(ansi-term :which-key "open terminal")
   "qq"  '(kill-emacs :which-key "quit")
 ))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-evil which-key rainbow-delimiters ivy doom-themes evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
