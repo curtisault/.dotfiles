608e7c7ceb3b63eafaeee8404df9a6e80d7825dc
Curtis Ault <curtisault@gmail.com>
2022-03-22 14:25:02 -0500

    Create colorscheme.lua

---
+++ .config/nvim/lua/config/colorscheme.lua
@@ -0,0 +1,12 @@
+-- Adapted from:
+-- https://alpha2phi.medium.com/neovim-init-lua-e80f4f136030
+local utils = require('utils')
+local cmd = vim.cmd
+utils.opt('o', 'termguicolors', true)
+utils.opt('o', 'background', 'dark')
+
+-- Added 2021-08-01, Kurt Symanzik
+vim.g['everforest_background'] = 'soft'
+vim.g['everforest_transparent_background'] = 0
+
+cmd 'colorscheme everforest'
