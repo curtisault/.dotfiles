c5d88d47dad873b3dd448817bf443105e2e8b68d
Curtis Ault <curtisault@gmail.com>
2022-03-22 14:23:23 -0500

    Update keymaps.lua

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,28 +1,45 @@
-local opts = { noremap = true, silent = true }
+local utils = require('utils')
 
-local term_opts = { silent = true }
+------------- Normal -------------
+utils.map('n', '<esc>', '<cmd>noh<CR>') -- Clear highlights
 
-local keymap = vim.api.nvim_set_keymap
+-- Window Split
+utils.map('n', '<leader>fs', '<cmd>w<CR>')
+utils.map('n', '<leader>wl', '<cmd>vsplit<CR>')
+utils.map('n', '<leader>wj', '<cmd>split<CR>')
+utils.map('n', '<leader>wd', '<cmd>close<CR>')
 
--- Remap space to leader
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Window Nav
+utils.map('n', '<C-h>', '<C-w>h<CR>')   
+utils.map('n', '<C-j>', '<C-w>j<CR>')
+utils.map('n', '<C-k>', '<C-w>k<CR>')
+utils.map('n', '<C-l>', '<C-w>l<CR>')
 
+-- Window Resize
+utils.map('n', '<S-Up>', '<cmd>resize -2<CR>')
+utils.map('n', '<S-Down>', '<cmd>resize +2<CR>')
+utils.map('n', '<S-Left>', '<cmd>vertical resize -2<CR>')
+utils.map('n', '<S-Right>', '<cmd>vertical resize +2<CR>')
 
------- Normal ------
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- Tab
+utils.map('n', 'tt', '<c-w>T')
+utils.map('n', 'th', '<cmd>tabfirst<CR>')
+utils.map('n', 'tj', '<cmd>tabnext<CR>')
+utils.map('n', 'tk', '<cmd>tabprev<CR>')
+utils.map('n', 'tl', '<cmd>tablast<CR>')
+utils.map('n', 'tn', '<cmd>tabnew<CR>')
+utils.map('n', 'td', '<cmd>tabclose<CR>')
 
--- resize
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- Adapted from: https://alpha2phi.medium.com/neovim-init-lua-e80f4f136030
+-- Find files using Telescope command-line sugar.
+-- Needed to install ripgrep and fd for some of these to work.
+utils.map('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+utils.map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+utils.map('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
+utils.map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
+utils.map('n', '<leader>fs', '<cmd>Telescope grep_string<cr>')
 
------- Visual ------
--- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+------------- Visual -------------
+utils.map('v', '<', '<gv')   -- Window Nav
+utils.map('v', '>', '>gv')   -- Window Nav
+
