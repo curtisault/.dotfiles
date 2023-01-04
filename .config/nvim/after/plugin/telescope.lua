2cc70f3c8909b1f79b1103124c84c10bb731c1b4
Curtis Ault <curtisault@gmail.com>
2023-01-04 11:59:51 -0600

    Update telescope.lua

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,6 +3,12 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ps', function()
+
+vim.keymap.set('n', '<leader>rg', function()
 	builtin.grep_string({ search = vim.fn.input("rg > ")});
 end)
+
+-- TODO: find_files w/ fd
+-- vim.keymap.set('n', '<leader>fd', function()
+-- 	builtin.find_files({ search = vim.fn.input("")});
+-- end)
