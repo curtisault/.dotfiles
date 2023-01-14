2ff99bc7cde5998d566d5b159afc0fc856cf97d2
Curtis Ault <curtisault@gmail.com>
2023-01-14 01:16:55 -0600

    Update lsp.lua

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -9,11 +9,26 @@ lsp.ensure_installed({
   'rust_analyzer'
 })
 
+-- open error on hover
+vim.api.nvim_create_autocmd('CursorHold', {
+    callback = function()
+        local opts = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = 'rounded',
+            source = 'always',
+            prefix = ' ',
+            scope = 'cursor',
+        }
+        vim.diagnostic.open_float(nil, opts)
+    end,
+})
+
 -- if a language server is running print info
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-  end,
+    callback = function(args)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+    end,
 })
 
 lsp.nvim_workspace()
