2f1415839d6c0631e22da30b48e4f9ba6487eb07
Curtis Ault <curtisault@gmail.com>
2023-07-27 10:43:09 -0500

    Update packer.lua

--- .config/nvim/lua/curtis/packer.lua
+++ .config/nvim/lua/curtis/packer.lua
@@ -28,8 +28,12 @@ return require('packer').startup(function(use)
 
   -- Language Parser
   use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = ':TSUpdate'
+          'nvim-treesitter/nvim-treesitter',
+          run = ':TSUpdate'
+  }
+
+  use { 'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
   }
 
   use {
