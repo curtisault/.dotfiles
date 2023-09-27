fa3680d744fa9af86ce59f090c09c1fcb53b2e93
Curtis Ault <curtisault@gmail.com>
2023-09-27 15:15:09 -0500

    Create lazy.lua

---
+++ .config/nvim/lua/curtis/lazy.lua
@@ -0,0 +1,54 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  -- Syntax highlighting
+  'sheerun/vim-polyglot',
+
+  -- Color scheme
+  'neanias/everforest-nvim',
+
+  -- Getting Fuzzy
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.3',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+
+  'tpope/vim-dispatch',
+  'tpope/vim-fugitive',
+  'tpope/vim-commentary',
+  'folke/neodev.nvim',
+
+  --- LSP Zero Setup
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
+
+  -- Language Parser
+  'nvim-treesitter/nvim-treesitter',
+
+  -- Statuses are so cool
+  { 'nvim-lualine/lualine.nvim',
+    dependencies = {'kyazdani42/nvim-web-devicons', opt = true}
+  },
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
