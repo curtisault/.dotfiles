f661d585f70097c329963a7bf03e4c3bab770b6f
Curtis Ault <curtisault@gmail.com>
2023-11-01 10:32:52 -0500

    Create heirline.lua

---
+++ .config/nvim/lua/curtis/heirline.lua
@@ -0,0 +1,301 @@
+-- FILE NOT IN USE. WANTED TO PLAY WITH BUILDING MY OWN STATUSLINE.
+
+local heirline = require("heirline")
+local conditions = require("heirline.conditions")
+local utils = require("heirline.utils")
+local Align = { provider = "%=" }
+local Space = { provider = " " }
+
+local colors = {
+    bright_bg = utils.get_highlight("Folded").bg,
+    bright_fg = utils.get_highlight("Folded").fg,
+    red = utils.get_highlight("DiagnosticError").fg,
+    dark_red = utils.get_highlight("DiffDelete").bg,
+    green = utils.get_highlight("String").fg,
+    blue = utils.get_highlight("Function").fg,
+    gray = utils.get_highlight("NonText").fg,
+    orange = utils.get_highlight("Constant").fg,
+    purple = utils.get_highlight("Statement").fg,
+    cyan = utils.get_highlight("Special").fg,
+    diag_warn = utils.get_highlight("DiagnosticWarn").fg,
+    diag_error = utils.get_highlight("DiagnosticError").fg,
+    diag_hint = utils.get_highlight("DiagnosticHint").fg,
+    diag_info = utils.get_highlight("DiagnosticInfo").fg,
+    git_del = utils.get_highlight("diffDeleted").fg,
+    git_add = utils.get_highlight("diffAdded").fg,
+    git_change = utils.get_highlight("diffChanged").fg,
+}
+
+local ViMode = {
+    -- get vim current mode, this information will be required by the provider
+    -- and the highlight functions, so we compute it only once per component
+    -- evaluation and store it as a component attribute
+    init = function(self)
+        self.mode = vim.fn.mode(1) -- :h mode()
+    end,
+    -- Now we define some dictionaries to map the output of mode() to the
+    -- corresponding string and color. We can put these into `static` to compute
+    -- them at initialisation time.
+    static = {
+        mode_names = { -- change the strings if you like it vvvvverbose!
+            n = "N",
+            no = "N?",
+            nov = "N?",
+            noV = "N?",
+            ["no\22"] = "N?",
+            niI = "Ni",
+            niR = "Nr",
+            niV = "Nv",
+            nt = "Nt",
+            v = "V",
+            vs = "Vs",
+            V = "V_",
+            Vs = "Vs",
+            ["\22"] = "^V",
+            ["\22s"] = "^V",
+            s = "S",
+            S = "S_",
+            ["\19"] = "^S",
+            i = "I",
+            ic = "Ic",
+            ix = "Ix",
+            R = "R",
+            Rc = "Rc",
+            Rx = "Rx",
+            Rv = "Rv",
+            Rvc = "Rv",
+            Rvx = "Rv",
+            c = "C",
+            cv = "Ex",
+            r = "...",
+            rm = "M",
+            ["r?"] = "?",
+            ["!"] = "!",
+            t = "T",
+        },
+        mode_colors = {
+            n = "red" ,
+            i = "green",
+            v = "cyan",
+            V =  "cyan",
+            ["\22"] =  "cyan",
+            c =  "orange",
+            s =  "purple",
+            S =  "purple",
+            ["\19"] =  "purple",
+            R =  "orange",
+            r =  "orange",
+            ["!"] =  "red",
+            t =  "red",
+        }
+    },
+    -- We can now access the value of mode() that, by now, would have been
+    -- computed by `init()` and use it to index our strings dictionary.
+    -- note how `static` fields become just regular attributes once the
+    -- component is instantiated.
+    -- To be extra meticulous, we can also add some vim statusline syntax to
+    -- control the padding and make sure our string is always at least 2
+    -- characters long. Plus a nice Icon.
+    provider = function(self)
+        return " %2("..self.mode_names[self.mode].."%)"
+    end,
+    -- Same goes for the highlight. Now the foreground will change according to the current mode.
+    hl = function(self)
+        local mode = self.mode:sub(1, 1) -- get only the first mode character
+        return { fg = self.mode_colors[mode], bold = true, }
+    end,
+    -- Re-evaluate the component only on ModeChanged event!
+    -- Also allows the statusline to be re-evaluated when entering operator-pending mode
+    update = {
+        "ModeChanged",
+        pattern = "*:*",
+        callback = vim.schedule_wrap(function()
+            vim.cmd("redrawstatus")
+        end),
+    },
+}
+
+local FileNameBlock = {
+    -- let's first set up some attributes needed by this component and it's children
+    init = function(self)
+        self.filename = vim.api.nvim_buf_get_name(0)
+    end,
+}
+
+local FileName = {
+    provider = function(self)
+        -- first, trim the pattern relative to the current directory. For other
+        -- options, see :h filename-modifers
+        local filename = vim.fn.fnamemodify(self.filename, ":.")
+        if filename == "" then return "[No Name]" end
+        -- now, if the filename would occupy more than 1/4th of the available
+        -- space, we trim the file path to its initials
+        -- See Flexible Components section below for dynamic truncation
+        if not conditions.width_percent_below(#filename, 0.25) then
+            filename = vim.fn.pathshorten(filename)
+        end
+        return filename
+    end,
+    hl = { fg = utils.get_highlight("Directory").fg },
+}
+
+local FileFlags = {
+    {
+        condition = function()
+            return vim.bo.modified
+        end,
+        provider = "[+]",
+        hl = { fg = "green" },
+    },
+    {
+        condition = function()
+            return not vim.bo.modifiable or vim.bo.readonly
+        end,
+        provider = "",
+        hl = { fg = "orange" },
+    },
+}
+
+local FileNameModifer = {
+    hl = function()
+        if vim.bo.modified then
+            -- use `force` because we need to override the child's hl foreground
+            return { fg = "cyan", bold = true, force=true }
+        end
+    end,
+}
+
+FileNameBlock = utils.insert(FileNameBlock,
+    utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
+    FileFlags,
+    { provider = '%<'} -- this means that the statusline is cut here when there's not enough space
+)
+
+local FileType = {
+    provider = function()
+        return string.upper(vim.bo.filetype)
+    end,
+    hl = { fg = utils.get_highlight("Type").fg, bold = true },
+}
+
+local FileEncoding = {
+    provider = function()
+        local enc = (vim.bo.fenc ~= '' and vim.bo.fenc) or vim.o.enc -- :h 'enc'
+        return enc ~= 'utf-8' and enc:upper()
+    end
+}
+
+local FileFormat = {
+    provider = function()
+        local fmt = vim.bo.fileformat
+        return fmt ~= 'unix' and fmt:upper()
+    end
+}
+
+local FileSize = {
+    provider = function()
+        -- stackoverflow, compute human readable file size
+        local suffix = { 'b', 'k', 'M', 'G', 'T', 'P', 'E' }
+        local fsize = vim.fn.getfsize(vim.api.nvim_buf_get_name(0))
+        fsize = (fsize < 0 and 0) or fsize
+        if fsize < 1024 then
+            return fsize..suffix[1]
+        end
+        local i = math.floor((math.log(fsize) / math.log(1024)))
+        return string.format("%.2g%s", fsize / math.pow(1024, i), suffix[i + 1])
+    end
+}
+
+local FileLastModified = {
+    -- did you know? Vim is full of functions!
+    provider = function()
+        local ftime = vim.fn.getftime(vim.api.nvim_buf_get_name(0))
+        return (ftime > 0) and os.date("%c", ftime)
+    end
+}
+
+-- We're getting minimalists here!
+local Ruler = {
+    -- %l = current line number
+    -- %L = number of lines in the buffer
+    -- %c = column number
+    -- %P = percentage through file of displayed window
+    provider = "%7(%l/%3L%):%2c %P",
+}
+
+-- I take no credits for this! :lion:
+local ScrollBar ={
+    static = {
+        sbar = { '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█' }
+        -- Another variant, because the more choice the better.
+        -- sbar = { '🭶', '🭷', '🭸', '🭹', '🭺', '🭻' }
+    },
+    provider = function(self)
+        local curr_line = vim.api.nvim_win_get_cursor(0)[1]
+        local lines = vim.api.nvim_buf_line_count(0)
+        local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
+        return string.rep(self.sbar[i], 2)
+    end,
+    hl = { fg = "blue", bg = "bright_bg" },
+}
+
+local LSPActive = {
+    condition = conditions.lsp_attached,
+    update = {'LspAttach', 'LspDetach'},
+
+    -- You can keep it simple,
+    -- provider = " [LSP]",
+
+    -- Or complicate things a bit and get the servers names
+    provider  = function()
+        local names = {}
+        for i, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+            table.insert(names, server.name)
+        end
+        return " [" .. table.concat(names, " ") .. "]"
+    end,
+    hl = { fg = "green", bold = true },
+}
+
+-- I personally use it only to display progress messages!
+-- See lsp-status/README.md for configuration options.
+
+-- Note: check "j-hui/fidget.nvim" for a nice statusline-free alternative.
+-- TODO: Figure out LSP
+-- local LSPMessages = {
+--     provider = require("lsp-status").status,
+--     hl = { fg = "gray" },
+-- }
+
+
+ViMode = utils.surround({ "", "" }, "bright_bg", { ViMode, Snippets })
+
+local DefaultStatusline = {
+    ViMode, Space, FileNameBlock, Space, Git, Align,
+    LSPActive, Space, FileType, Space, Ruler, Space, ScrollBar
+}
+
+local InactiveStatusline = {
+    condition = conditions.is_not_active,
+    FileType, Space, FileName, Align,
+}
+
+local StatusLines = {
+
+    hl = function()
+        if conditions.is_active() then
+            return "StatusLine"
+        else
+            return "StatusLineNC"
+        end
+    end,
+
+    -- the first statusline with no condition, or which condition returns true is used.
+    -- think of it as a switch case with breaks to stop fallthrough.
+    fallthrough = false,
+
+    InactiveStatusline, DefaultStatusline
+}
+
+require("heirline").setup({ statusline = StatusLines })
+
