6491dc5a9aefc2ea11cec22fe9f4ddef9027ee9f
Curtis Ault <curtisault@gmail.com>
2024-05-15 09:38:22 -0500

    Update lazy.lua

--- .config/nvim/lua/curtis/lazy.lua
+++ .config/nvim/lua/curtis/lazy.lua
@@ -16,18 +16,24 @@ local plugins = {
     -- Syntax highlighting
     'sheerun/vim-polyglot',
 
-    -- Color scheme
+    -- Color schemes
     'neanias/everforest-nvim',
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000
-    },
+    { 'ellisonleao/gruvbox.nvim', priority = 1000 },
+    { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
 
     -- Getting Fuzzy
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.3',
+        tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+
+    -- Explore but more better
+    {
+      'stevearc/oil.nvim',
+      opts = {},
+      -- Optional dependencies
+      dependencies = { "nvim-tree/nvim-web-devicons" },
     },
 
     'tpope/vim-dispatch',
@@ -36,22 +42,20 @@ local plugins = {
     'folke/neodev.nvim',
 
     --- LSP Zero Setup
+    { 
+        'VonHeikemen/lsp-zero.nvim', 
+        branch = 'v3.x' 
+    },
+
+    -- LSP Support
+    { 'neovim/nvim-lspconfig' },
     { 'williamboman/mason.nvim' },
     { 'williamboman/mason-lspconfig.nvim' },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        lazy = true,
-        config = false
-    },
-    { 'neovim/nvim-lspconfig' },
 
-    -- completion
-    { 'hrsh7th/cmp-nvim-lsp' },
+    -- Autocompletion
     { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
     { 'L3MON4D3/LuaSnip' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'rafamadriz/friendly-snippets' },
 
     -- Language Parser
     'nvim-treesitter/nvim-treesitter',
@@ -60,6 +64,52 @@ local plugins = {
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'kyazdani42/nvim-web-devicons', opt = true }
+    },
+
+    -- pomodoro timer for timeboxing tasks
+    {
+        'epwalsh/pomo.nvim',
+        version = '*', -- Recommended, use latest release instead of latest commit
+        lazy = true,
+        cmd = { 'TimerStart', 'TimerRepeat' },
+        dependencies = {
+            -- Optional, but highly recommended if you want to use the "Default" timer
+            'rcarriga/nvim-notify',
+        },
+        opts = {
+            -- See below for full list of options 👇
+        },
+    },
+
+    -- NOICE - Lazy notifications
+    {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+        }
+    },
+
+    -- Copilot
+    'github/copilot.vim',
+
+    -- Cody AI
+    {
+        {
+            'sourcegraph/sg.nvim',
+            dependencies = { 'nvim-lua/plenary.nvim', --[[ 'nvim-telescope/telescope.nvim' ]] },
+
+            -- If you have a recent version of lazy.nvim, you don't need to add this!
+            build = 'nvim -l build/init.lua',
+        },
     },
 }
 
