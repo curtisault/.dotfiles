89c808c18a1df222ad1914a9fb7e068f06c51f44
Curtis Ault <curtisault@gmail.com>
2025-01-19 00:30:11 -0600

    Update lazy.lua

--- .config/nvim/lua/curtis/lazy.lua
+++ .config/nvim/lua/curtis/lazy.lua
@@ -13,22 +13,15 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    -- Syntax highlighting
-    'sheerun/vim-polyglot',
+    -- Syntax highlighting (unsure if needed)
+    -- 'sheerun/vim-polyglot',
+
+    { 'echasnovski/mini.nvim', version = false },
 
     -- Color schemes
     'neanias/everforest-nvim',
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     { "ellisonleao/gruvbox.nvim", priority = 1000 },
-    { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
-    { "EdenEast/nightfox.nvim" },
-
-    -- Getting Fuzzy
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.6',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
 
     -- Explore but more better
     {
@@ -40,7 +33,6 @@ local plugins = {
 
     'tpope/vim-dispatch',
     'tpope/vim-fugitive',
-    'tpope/vim-commentary',
     'folke/neodev.nvim',
 
     --- LSP Zero Setup
@@ -83,23 +75,6 @@ local plugins = {
         },
     },
 
-    -- NOICE - Lazy notifications
-    {
-      "folke/noice.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- add any options here
-      },
-      dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
-        }
-    },
-
     -- MARKDOWN
     {
       'MeanderingProgrammer/render-markdown.nvim',
@@ -109,17 +84,6 @@ local plugins = {
 
     -- Copilot
     'github/copilot.vim',
-
-    -- Cody AI
-    {
-        {
-            'sourcegraph/sg.nvim',
-            dependencies = { 'nvim-lua/plenary.nvim', --[[ 'nvim-telescope/telescope.nvim' ]] },
-
-            -- If you have a recent version of lazy.nvim, you don't need to add this!
-            build = 'nvim -l build/init.lua',
-        },
-    },
 }
 
 local opts = {}
